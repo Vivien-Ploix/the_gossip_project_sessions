52ac478280dd1f6d8cb83fce7260de45a9a03c12
Vivien-Ploix <vivien.ploix@gmail.com>
2020-08-05 16:22:53 +0200

    edit gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -15,8 +15,8 @@ class GossipsController < ApplicationController
     @post = Gossip.new(title: params[:title], content:params[:content], user_id: id)
 
     if @post.save
-      @message = "The super potin was succesfully saved !"
-      flash[:success] = "The super potin was succesfully saved !"
+      @message = "The gossip was succesfully saved !"
+      flash[:success] = "The gossip was succesfully saved !"
       redirect_to root_path
     else
       @alert = true
@@ -25,7 +25,22 @@ class GossipsController < ApplicationController
     end
   end
 
+  def edit 
+    @gossip = Gossip.find(params[:id])
+  end 
 
+  def update
+    @gossip = Gossip.find(params[:id])
+    gossip_params = params.require(:gossip).permit(:title, :content)
 
+    if @gossip.update(gossip_params)
+      flash[:success] = "The gossip was succesfully updated !"
+      redirect_to gossip_path(@gossip.id)
+    else
+      @alert = true
+      @message = "Error: " + @gossip.errors.messages.to_a.flatten[1]
+      render "edit"
+    end
+  end
 
 end
